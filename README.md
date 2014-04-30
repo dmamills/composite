c8aedabfe219c37e7906f33824622f9dca12ab92
Daniel Mills <mills.dma@gmail.com>
2014-04-29 22:07:45 -0400

    inital

---
+++ README.md
@@ -0,0 +1,32 @@
+# Composite
+
+A composer package that wraps the [Composite](http://www.imagemagick.org/script/composite.php) command. Composite is part of the image editing software [ImageMagick](http://www.imagemagick.org/index.php).
+
+## Usage
+
+```
+
+use Composite\Composite;
+
+$composite = new Composite('base.png','overlay.png','result.png');
+$composite->execute();
+
+$resultFile = $composite.getResultFile(); //
+$resultFile = $composite.getResultFile(true); //calls execute() before retrieving file
+
+```
+
+It also can take a fourth parameter to set command options
+
+```
+
+$composite = new Composite('base.png','overlay.png','result.png',array(
+	'gravity'=>'southeast',
+	'size'=>'512x512'
+));
+
+```
+
+## License
+
+MIT 
---
+++ src/Composite/Composite.php
@@ -0,0 +1,94 @@
+<?php
+namespace Composite;
+
+
+/*
+
+*/
+class Composite {
+	
+	public function __construct($base,$overlay,$outfile,$options = array()) 
+	{
+		$this->base = $base;
+		$this->overlay = $overlay;
+		$this->outfile = $outfile;
+
+		$this->options = $options;
+		$this->executed = false;
+	}
+
+	/*
+		Executes the composite command
+	*/
+	public function execute()
+	{
+
+		if(!file_exists($this->overlay)) {
+			throw new Exception('Overlay image not found.');
+		}
+
+		if(!file_exists($this->base)) {
+			throw new Exception('Base image not found.');
+		}		
+
+		$results = array();
+		$command = Composite::prepareCommandString();
+		exec($command,$results);
+		$this->executed = true;
+
+		//parse results for errors/success
+
+		return $results;
+	}
+
+	public function getOverlayFile()
+	{
+		if(!file_exists($this->overlay))
+		{
+			throw new Exception('Overlay not found.');	
+		}
+
+		return fopen($this->overlay,'r+');
+	}
+
+
+	/*
+		Returns the created image as a file
+		if $execute is true it will execute the command first
+		if the command has not been executed an exception is thrown
+	*/
+	public function getOutFile($execute = false)
+	{
+		if($execute) 
+		{
+			$this->execute();
+		}
+
+		if(!$this->executed) throw new Exception('Composite not executed.');
+		$file = fopen($this->outfile,'r+');
+		return $file;
+	}
+
+
+	/*
+		Creates the composite string for execute
+	*/
+	private function prepareCommandString()
+	{
+		//composite -gravity center smile.gif rose: rose-over.png
+		$command = 'composite ';
+
+		foreach($this->options as $key => $value)
+		{
+			if($value != null)
+				$command = $command.'-'.$key.' '.$value.' ';
+			else
+				$command = $command.'-'.$key.' ';
+		}
+
+		$command = $command.$this->overlay.' '.$this->base.' '.$this->outfile;
+		echo 'Executing command: '.$command;
+		return $command;
+	}
+	
+}
